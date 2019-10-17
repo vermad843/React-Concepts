1f95155ff1a7dee5463054e90635fb27c982a2c2
vermad843 <vishnuverma7524@gmail.com>
2019-10-17 10:30:51 +0530

    importing App to index.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+function App() {
+  return (
+    <div>
+       <h1>Vishnu</h1>
+       <p>I Love Coding</p>   
+      <ul>
+        <li>London </li>
+        <li>Paris</li>
+        <li>NewYork</li>
+     </ul>
+   </div>
+  );   
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-
-
-function App() {
-  return (
-    <div>
-      <ul>
-        <li>work </li>
-        <li>work harder</li>
-        <li>hardest</li>
-     </ul>
-  </div>
-  );   
-}
+import App from './App';
 
 
 
 
-
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render( < App/ > , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
