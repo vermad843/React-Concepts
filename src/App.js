d48524fa8187029913e241b1ed20c281f19abac8
vermad843 <vishnuverma7524@gmail.com>
2019-10-19 14:59:54 +0530

    Best player in the world form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Nav from './components/Nav';
-import Main from './components/main';
-import Footer from './components/footer';
+import React, { Component } from 'react';
 import './App.css';
 
 
 
+class App extends Component {
+    render() {
+        return (
+          <div className = "title">
+          <h3> Best Player in the World</h3>
+              <input type = "checkbox" />
+              <p>C.Ronaldo </p>
 
-function App() {
-  return (
-    <div className = "App">
-      <Nav/>
-      <Main/>
-      <Footer/>
-   </div>
-  );   
+              <input type = "checkbox" />
+              <p>Messi </p>
+              
+              <input type = "checkbox" />
+              <p>Neymar </p>
+
+              <input type = "checkbox" />
+              <p>Ronaldino </p>
+          </div>            
+        );
+    }
 }
-
 
 export default App;
