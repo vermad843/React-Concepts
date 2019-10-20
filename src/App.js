6c6137072d070c89e130dc2c5deb4be090fafc05
vermad843 <vishnuverma7524@gmail.com>
2019-10-20 12:49:45 +0530

    adding image to the page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import ContactCard from './ContactCard';
 
-
-
-class App extends Component {
-    render() {
-        return (
-          <div className = "title">
-          <h3> Best Player in the World</h3>
-              <input type = "checkbox" />
-              <p>C.Ronaldo </p>
-
-              <input type = "checkbox" />
-              <p>Messi </p>
-              
-              <input type = "checkbox" />
-              <p>Neymar </p>
-
-              <input type = "checkbox" />
-              <p>Ronaldino </p>
-          </div>            
-        );
-    }
+function App() {
+    return (
+      <div className = "Contacts">
+        <div className = "contact-card">
+           <img className = "Image" src = "youtube-3873587_1920.jpg"/>
+           <h3>Youtube</h3> 
+           <p>phone : 000001</p>
+           <p>Email : Youtube</p>
+        </div>
+        <div className = "contact-card">
+           <img className = "Image" src = "youtube-3873587_1920.jpg"/>
+           <h3>Youtube</h3> 
+           <p>phone : 000001</p>
+           <p>Email : Youtube</p>
+        </div>
+        <div className = "contact-card">
+           <img className = "Image" src = "youtube-3873587_1920.jpg"/>
+           <h3>Youtube</h3> 
+           <p>phone : 000001</p>
+           <p>Email : Youtube</p>
+        </div>
+        <div className = "contact-card">
+           <img className = "Image" src = "youtube-3873587_1920.jpg"/>
+           <h3>Youtube</h3> 
+           <p>phone : 000001</p>
+           <p>Email : Youtube</p>
+        </div>
+      </div>
+    )
 }
 
 export default App;
---
+++ src/ContactCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ContactCard() {
+    return (
+        <div className = "contact-card">
+           <img className = "Image" src = "youtube-3873587_1920.jpg"/>
+           <h3>Youtube</h3> 
+           <p>phone : 000001</p>
+           <p>Email : Youtube</p>
+        </div>
+    )
+}
+
+export default ContactCard;
