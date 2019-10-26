9a51dd15873fc9a2af287ee7e3e934d22bd6ccb4
vermad843 <vishnuverma7524@gmail.com>
2019-10-26 14:09:33 +0530

    adding MyInfo component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function MyInfo() {
-    return (
-      <div>
-        <h1 >Vishnu</h1>
-        <p>I Love Coding!!!!</p>
-        <ul>
-           <li>London</li>
-           <li>Paris</li>
-           <li>NewYork</li>
-       </ul>
-    </div> 
-    );
-}
+import MyInfo from './MyInfo';
 
 
 
