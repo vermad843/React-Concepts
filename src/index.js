5f4f7e4eafc469ce455715a20f740a46d21126af
vermad843 <vishnuverma7524@gmail.com>
2019-10-26 13:40:34 +0530

    error only one element can be rendered

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>hello world</h1>, document.getElementById("root"))
+ReactDOM.render(<h1>hello world</h1><p> this is a paragraph</p>
+    , document.getElementById("root"))
