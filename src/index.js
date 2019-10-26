04cfe1a6c2e3ec93906b16102bc262ea70c3f440
vermad843 <vishnuverma7524@gmail.com>
2019-10-26 13:54:44 +0530

    creating MyApp function and returning <ul></ul>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <ul>
+function MyApp() {
+    return (
+     <ul>
         <li>1</li>
         <li>2</li>
         <li>3</li>
     </ul>
+    );
+}
+
+
+
+ReactDOM.render(
+  <MyApp/>
     , document.getElementById("root"))
