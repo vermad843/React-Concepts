d82c96d56020082ee6185b05a7e6b81fd19f5d62
vermad843 <vishnuverma7524@gmail.com>
2019-10-26 16:08:11 +0530

    changing the color according to time

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import React from 'react';
 import './App.css';
 
 function App() {
-   const date = new Date()
+   const date = new Date(2019,10,25,10)//year/month/date/time
    const hours = date.getHours()
    let timeOfDay
+   
+   const styles = {
+    fontSize : "100px"
+   }
 
    if(hours < 12) {
      timeOfDay = ' morning'
+     styles.color = "red";
    }else if (hours >= 12 && hours < 17) {
      timeOfDay = ' afternoon'
+     styles.color = "green";
    }else {
      timeOfDay = ' night'
+     styles.color = "blue";
    }
 
-   const styles = {
-     color : "orange",
-     backgroundColor : "red",
-     fontSize : "100px"
-    }
    return (
      <h1 style = {styles}>
        Good{timeOfDay}
