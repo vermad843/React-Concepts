8a08b3db7aaa81539c592e0ee33260797645d005
vermad843 <vishnuverma7524@gmail.com>
2019-10-26 14:47:17 +0530

    adding checkBoxes form

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className = "player">
+      <h1>Best player in the World!!</h1>
+      <input type = "checkbox" />
+      <p>Cristiano Ronaldo</p>
+      <input type = "checkbox" />
+      <p>Lionel Messi</p> 
+      <input type = "checkbox" />
+      <p>Neymar </p> 
+      <input type = "checkbox" />
+      <p>Ronaldino</p>
+    </div>
+  );
+}
+
+export default App; 
