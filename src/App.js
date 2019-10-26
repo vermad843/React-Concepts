086ca728307b27fb1932ff274f710ec7a85cfcfb
vermad843 <vishnuverma7524@gmail.com>
2019-10-26 16:32:21 +0530

    adding some styles

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import './App.css';
+import TodoItem from './TodoItem'
 
 function App() {
-   const date = new Date(2019,10,25,10)//year/month/date/time
-   const hours = date.getHours()
-   let timeOfDay
-   
-   const styles = {
-    fontSize : "100px"
-   }
-
-   if(hours < 12) {
-     timeOfDay = ' morning'
-     styles.color = "red";
-   }else if (hours >= 12 && hours < 17) {
-     timeOfDay = ' afternoon'
-     styles.color = "green";
-   }else {
-     timeOfDay = ' night'
-     styles.color = "blue";
-   }
-
    return (
-     <h1 style = {styles}>
-       Good{timeOfDay}
-      </h1>
-  );
+     <div className = "todo-list">
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+     </div>
+   )
 }
 
-export default App; 
+export default App;
