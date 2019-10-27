08d90836c97dda08d496f59462f1491b10f57140
vermad843 <vishnuverma7524@gmail.com>
2019-10-27 15:39:31 +0530

    adding checked property in input

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TodoItem(props) {
   return (
       <div className = "todo-item">
-          <input type = "checkbox"/>
+          <input type = "checkbox" checked = {props.item.completed}/>
           <p>{props.item.text}</p>
       </div>
   );
