3fceaa7bd309a441536ab1e61a101e91bc7f7c88
vermad843 <vishnuverma7524@gmail.com>
2019-10-28 09:27:24 +0530

    components in a same page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
 
 
-// function App() {
-//   return (
-//    <div>
-//        <h1>{props.whatever}</h1>
-//    </div>
-//   );
-// }
-
-
-
-
-
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>{this.props.whatever}</h1>
+         <Header/>
+         <Greeting/>
       </div>
     );
   }
 }
 
 export default App;
+
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+         <p>welcome, {this.props.username}!!!</p>
+      </header>
+    );
+  }
+}
+
+
+class Greeting extends Component {
+  render() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    if(hours < 12) {
+      timeOfDay = "morning"
+    }else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon"
+    }else {
+      timeOfDay = "night"
+    }
+    return (
+       <div>
+           <h1>Good {timeOfDay} to you,sir or madam</h1> 
+       </div>
+    );
+  }
+}
+
