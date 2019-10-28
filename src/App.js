98335bfe45480a462107215933f1df64dc8dcf60
vermad843 <vishnuverma7524@gmail.com>
2019-10-28 09:14:16 +0530

    use of props in class base component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
+
+// function App() {
+//   return (
+//    <div>
+//        <h1>{props.whatever}</h1>
+//    </div>
+//   );
+// }
+
+
+
+
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World!!!</h1>
+        <h1>{this.props.whatever}</h1>
       </div>
     );
   }
