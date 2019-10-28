8e1f97526ee78a7ad8e3d821522d6f6e3a0c9d7c
vermad843 <vishnuverma7524@gmail.com>
2019-10-28 09:37:53 +0530

    accessing state property inside the return div

--- src/App.js
+++ src/App.js
@@ -2,48 +2,19 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      answer : "yes"
+      };
+  }
   render() {
     return (
       <div>
-         <Header username = "Vishnu"/>
-         <Greeting/>
+         <h1>Is state important to know ?{this.state.answer} </h1>
       </div>
     );
   }
 }
 
 export default App;
-
-
-class Header extends Component {
-  render() {
-    return (
-      <header>
-         <p>welcome, {this.props.username}!!!</p>
-      </header>
-    );
-  }
-}
-
-
-class Greeting extends Component {
-  render() {
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay
-
-    if(hours < 12) {
-      timeOfDay = "morning"
-    }else if (hours >= 12 && hours < 17) {
-      timeOfDay = "afternoon"
-    }else {
-      timeOfDay = "night"
-    }
-    return (
-       <div>
-           <h1>Good {timeOfDay} to you,sir or madam</h1> 
-       </div>
-    );
-  }
-}
-
