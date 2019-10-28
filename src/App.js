e0eedd574d77b0aa7c9fd00918c9a56f6e0333cd
vermad843 <vishnuverma7524@gmail.com>
2019-10-28 09:43:49 +0530

    state practice

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 
-
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      answer : "yes"
-      };
+  constructor(props) {
+    super(props);
+    this.state = { 
+      name : 'Vishnu',
+      age : 23
+     };
   }
   render() {
     return (
-      <div>
-         <h1>Is state important to know ?{this.state.answer} </h1>
-      </div>
+       <div>
+          <h1>{this.state.name}</h1>
+          <h3>{this.state.age} years old</h3>
+       </div>
     );
   }
 }
