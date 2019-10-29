b40717940304ff807d4b819e1455a75621611371
vermad843 <vishnuverma7524@gmail.com>
2019-10-29 09:28:04 +0530

    onMouseOver event

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import './App.css'
+import './App.css';
  
 
 function handleClick() {
   console.log('here i am !!!')
 }
+ 
 
+function hover() {
+  console.log('hover')
+}
 
 function App() {
   return (
    <div>
-      <img src = "cr.jpg" className = "image" alt = "cr"/>
+      <img onMouseOver = {hover} src = "cr.jpg" className = "image" alt = "cr"/>
       <br/>
       <br/>
       <button onClick = {handleClick}>Click Me</button>
