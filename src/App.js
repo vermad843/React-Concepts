290f96f5bf67353eba1b0a93427c1a70acff8f15
vermad843 <vishnuverma7524@gmail.com>
2019-10-29 09:39:47 +0530

    logging selected when checkboxes are clicked

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
+import TodoItem from './TodoItem';
+import todosData from './todosData';
 import './App.css';
- 
+const todoItems = todosData.map((item) => {
+  return <TodoItem key = {item.id} item = {item}/>
+})
 
-function handleClick() {
-  console.log('here i am !!!')
-}
- 
-
-function hover() {
-  console.log('hover')
-}
 
 function App() {
-  return (
-   <div>
-      <img onMouseOver = {hover} src = "cr.jpg" className = "image" alt = "cr"/>
-      <br/>
-      <br/>
-      <button onClick = {handleClick}>Click Me</button>
-   </div>
-  );
+ return (
+  <div className = "todo-list">
+  {todoItems}
+</div> 
+ );
 }
 
-export default App
 
-
+export default App;
