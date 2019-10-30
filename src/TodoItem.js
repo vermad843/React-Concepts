3c5ae3c2ba0c83d308cd22afa27e42919158ce3c
vermad843 <vishnuverma7524@gmail.com>
2019-10-30 19:29:27 +0530

    todo phase 7

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 
 
 class TodoItem extends Component {
-
     render() {
+          
+    const completedStyle = {
+        fontStyle : 'italic',
+        color : '#cdcdcd',
+        // textDecoration : 'line-through'
+    }
         return (
            <div>
             <input 
             type = "checkbox" 
             checked = {this.props.todos.completed} 
             onChange = {() =>this.props.handleChange(this.props.todos.id)} />
-            <p> {this.props.todos.text}</p>
+            <p style = {this.props.todos.completed ? null : completedStyle}> {this.props.todos.text}</p>
           </div>    
         );
     }
