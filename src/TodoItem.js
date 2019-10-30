5b48f7f905d5ca328a6a1a3e577d96231926518e
vermad843 <vishnuverma7524@gmail.com>
2019-10-30 18:36:17 +0530

    todo phase 5

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 
 
 class TodoItem extends Component {
+
+   checked () {
+       console.log('Changed!!!')
+   } 
+
+
     render() {
         return (
            <div>
-            <input type = "checkbox" checked = {this.props.todos.completed}  />
+            <input 
+            type = "checkbox" 
+            checked = {this.props.todos.completed} 
+            onChange = {this.checked} />
             <p> {this.props.todos.text}</p>
           </div>    
         );
