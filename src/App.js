0a3deaf3c96c357644714f0f65f13947497e702c
vermad843 <vishnuverma7524@gmail.com>
2019-10-31 20:44:46 +0530

    componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
+// Conditional rendering 
 import React, { Component } from 'react';
-import TodoItem from './TodoItem';
-import todosData from './todosData';
-import './App.css';
+import Conditional from './Conditional'
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            todos : todosData
+           isLoading : true 
         }
-        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange(id) {
-        this.setState((prevState) => {
-            const updatedTodos = prevState.todos.map((todo) => {
-                if(todo.id === id) {
-                    todo.completed = !todo.completed
-                }
-                return todo
-            })
-            return {
-                todos : updatedTodos
-            }
-         })   
-        }
-
+    componentDidMount() {
+        setTimeout(() => {
+           this.setState({
+               isLoading : false
+           }) 
+        }, 1000);
+    }
     render() {
-        const {todos} = this.state;
-      const todoItems = todos.map((todos) => {
-          return <TodoItem 
-          key = {todos.id} 
-          todos  = {todos} 
-          handleChange = {this.handleChange}/>
-      });
+        const {isLoading} = this.state;
         return (
-            <div className = "list">
-              <h1>Best Player In The World!!!*</h1>
-               {todoItems}
+            <div>
+                <Conditional isLoading ={isLoading}/>
             </div>
-            
         );
     }
 }
---
+++ src/Conditional.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Conditional extends Component {
+    render() {
+        return (
+         <h1>Temp</h1>   
+        );
+    }
+}
+
+export default Conditional;
