97ff8d1e0d4c4427dfc467953e9f534289c4b007
vermad843 <vishnuverma7524@gmail.com>
2019-11-05 08:12:58 +0530

    handleChange method

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            character : {},
-            loading : false 
-         };
-    }
 
-   componentDidMount() {
-       this.setState({
-           loading : true
-       })
-   fetch("https://swapi.co/api/people/1")
-      .then(res => res.json())
-      .then(data => { 
-          this.setState({
-          character : data ,
-          loading : false
-      })
-    })   
-}  
+class App extends Component {
+   constructor() {
+       super();
+       this.state = {
+
+       }
+       this.handleChange = this.handleChange.bind(this);
+   }
+ handleChange(event) {
+   this.setState({
+       firstName : event.target.value
+   })
+ }
 
 
     render() {
-        const {character , loading} = this.state;
-        const text = loading ? 'loading...' : character.name 
         return (
-            <div>
-              {text}   
-            </div>
+           <form>
+               <input 
+                type = "text" 
+                placeholder = "First Name"
+                onChange = {this.handleChange}
+                />
+           </form> 
         );
     }
 }
