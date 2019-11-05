fddfc688f745ae0b484bf0706b5c43cf9088e36f
vermad843 <vishnuverma7524@gmail.com>
2019-11-05 07:49:31 +0530

    getting the character name on the screen

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import React, { Component } from 'react';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = {
+            character : {}
+         };
     }
 
    componentDidMount() {
    fetch("https://swapi.co/api/people/1")
       .then(res => res.json())
-      .then(data => console.log(data))
-   }
+      .then(data => { 
+          this.setState({
+          character : data 
+      })
+    })   
+}  
+
 
     render() {
+        const {character} = this.state;
         return (
-            <h1>Fetching the data</h1>
+            <div>
+            {character.name } 
+            </div>
         );
     }
 }
